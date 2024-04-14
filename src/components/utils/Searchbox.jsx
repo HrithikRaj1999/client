62fcf2fa9de04376ce351580d14a66a05206bbdd
Hrithik Raj <hrkraj99@gmail.com>
2024-04-14 15:17:23 +0530

    initial commit

---
+++ src/components/utils/Searchbox.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useSearch } from "../../context/searchContext";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const Searchbox = () => {
+  const [values, setValues] = useSearch();
+  const navigate = useNavigate();
+  const [searchText, setSearchText] = useState("");
+  const handleSearch = async () => {
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API}/api/v1/product/search/${searchText}`
+      );
+      setValues({ ...values, results: data });
+      navigate("/search");
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  return (
+    <div className="mb-3">
+      <div className=" flex justify-center">
+        <input
+          className="bg-stone-100 mx-4 rounded-2xl py-2 px-3 w-[200px] border-s-black-50 text-black"
+          placeholder="Search"
+          value={values.keyword}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="bg-blue-600 rounded-2xl mx-3 p-2 "
+          onClick={handleSearch}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Searchbox;
